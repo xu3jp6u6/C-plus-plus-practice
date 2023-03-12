cfe16353bf523ee9217cfe5aafa95c62f3371a83
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:30:35 +0800

    Add files via upload

---
+++ c007.cpp
@@ -0,0 +1,21 @@
+﻿// c007.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int a = 5, b = 3;
+    cout << "num = " << (a++) + b << " ,a = " << a << " ,b = " << b << endl;
+    a= 5, b = 3;
+    cout << "num = " << (++a) + b << " ,a = " << a << " ,b = " << b << endl;
+    a = 5, b = 3;
+    cout << "num = " << (a++) + (b++) << " ,a = " << a << " ,b = " << b << endl;
+    a = 5, b = 3;
+    cout << "num = " << (++a) + (++b) << " ,a = " << a << " ,b = " << b << endl;
+    a = 5, b = 3;
+    a += a + (b++);
+    cout << "a = " << a << " ,b = " << b << endl;
+}
+
