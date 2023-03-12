8870cd57f63ef7bda28276b01e39deac29521067
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:54:56 +0800

    Add files via upload

---
+++ c022.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+class caaa 
+{
+public:
+	int a;
+	int b;
+	int c;
+};
+int main()
+{
+	caaa obj;
+	obj.a = 1;
+	obj.b = 3;
+	obj.c = obj.a + obj.b;
+	cout << "a = " << obj.a << ",b = " << obj.b << ",c = " << obj.c;
+}
+
