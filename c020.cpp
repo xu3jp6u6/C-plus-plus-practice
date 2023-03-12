0206d3837b4594e3b5e61dccfb33ef1937767f4b
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:54:00 +0800

    Add files via upload

---
+++ c020.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+int main()
+{
+    char str[100];
+    char *ptr = str;
+    cin >> str;
+
+    // 將字串轉換為大寫
+    while (*ptr != '\0')
+    {
+        if (*ptr >= 'a' && *ptr <= 'z')
+        {
+            *ptr -= 32;
+        }
+        ptr++;
+    }
+
+    cout << "大寫形式：" << str << endl;
+}
