ad2e905e4b5036d9b217d1029ebcab4ea312904b
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:52:47 +0800

    Add files via upload

---
+++ c017.cpp
@@ -0,0 +1,24 @@
+﻿// c017.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[10];
+	int b;
+	float x=0;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "輸入 : ";
+		cin >> a[i];
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "&a[" << i << "]=" << &a[i] << endl;
+	}
+	cout << "&b=" << &b << endl;
+	cout << "&a=" << &a << endl;
+}
+
+
