ce6f9e4165554fc37a1af0813d904a28e85d316e
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:30:56 +0800

    Add files via upload

---
+++ c008.cpp
@@ -0,0 +1,14 @@
+﻿// c008.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+#define i a +10
+int main()
+{
+	char b = 'a';
+	int a = 36;
+	cout << i << endl;
+	cout << a*b << endl;
+}
+
