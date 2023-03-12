b44b7d4e1eeda35e5da6cf6359ab0a92b71cc8f2
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:56:02 +0800

    Add files via upload

---
+++ c024.cpp
@@ -0,0 +1,37 @@
+﻿// c024.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+class calculator
+{   public:
+    int a;
+    int b;
+    void add()
+    {
+        cout << a + b<<" ";
+    }
+    void sub()
+    {
+        cout << a - b << " ";
+    }
+    void mul()
+    {
+        cout << a * b << " ";
+    }
+    void div()
+    {
+        cout << a / b;
+    }
+};
+int main()
+{
+    calculator i;
+    i.a = 42;
+    i.b = 6;
+    i.add();
+    i.sub();
+    i.mul();
+    i.div();
+}
+
