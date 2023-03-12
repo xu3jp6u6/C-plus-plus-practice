d1f59b52e2f9d911cacadfe48f7861e1b5e97df9
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:53:15 +0800

    Add files via upload

---
+++ c018.cpp
@@ -0,0 +1,24 @@
+﻿// c018.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+void square(int*);
+int main()
+{
+    int a[3] = {2,5,7};
+    int* ptr=a;
+    for (int i = 0;i<3;i++)
+    {    
+        square(ptr+i);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        cout << a[i] <<" ";
+    }
+    
+}
+void square(int* arr)
+{
+    *arr *= *arr;
+}
