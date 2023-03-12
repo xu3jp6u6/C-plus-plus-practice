590519a47a0713c1fe1efb1adc62010616a58160
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:28:32 +0800

    Add files via upload

---
+++ c002.cpp
@@ -0,0 +1,13 @@
+﻿// c002.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    char beep = '\a';
+    int i = beep;
+    cout << "beep=" << beep;
+    cout << i << endl;
+    return 0;
+}
