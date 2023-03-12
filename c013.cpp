f3e18b3fef2289a86c9491c5fb8b0db373327d57
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:50:51 +0800

    Add files via upload

---
+++ c013.cpp
@@ -0,0 +1,13 @@
+﻿// c013.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float f;
+	int sum = 100;
+	cout <<  "f =" << f <<  ",sizeof(f) =" << sizeof(f) << ",位址為" << &f <<endl;
+	cout << "sum =" << sum << ",sizeof(sum) =" << sizeof(sum) << ",位址為" << &sum<<endl;
+}
+
