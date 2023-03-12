4df0ebe8af968eec1dbcc5d987da475afd116c53
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:52:19 +0800

    Add files via upload

---
+++ c016.cpp
@@ -0,0 +1,18 @@
+﻿// c016.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+double inch(int *);
+int main()
+{
+    int x;
+    cout << "輸入cm :";
+    cin >> x;
+    cout << inch(&x) << " inch";
+}
+double inch(int *p1)
+{
+    return *p1 * 0.394;
+}
+
