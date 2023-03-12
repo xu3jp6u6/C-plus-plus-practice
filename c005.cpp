598115c2721520866ee7376dc70c58efeb5bb6f1
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:29:56 +0800

    Add files via upload

---
+++ c005.cpp
@@ -0,0 +1,14 @@
+﻿// c005.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	char c = '76';
+	cin >> a >> b;
+	cout << "b-a=" << a - b << endl;
+	cout << "ch=" << c << endl;
+}
+
