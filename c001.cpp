8b1c874d30c47c48673815ea08446003db9bc31e
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:28:01 +0800

    Add files via upload

---
+++ c001.cpp
@@ -0,0 +1,15 @@
+﻿// c001.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+    int a;
+    cout << "how many ";
+    cin >> a;
+    cout << "You have " << a << " dog!" << "\n";
+    return 0;
+
+}
