6ef586dc28f4ef4f2f080241364e7216e448685a
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:45:18 +0800

    Add files via upload

---
+++ c012.cpp
@@ -0,0 +1,17 @@
+﻿// c012.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return(sum(n - 1) + n);
+}
+int  main()
+{
+    cout << sum(9);
+}
+
