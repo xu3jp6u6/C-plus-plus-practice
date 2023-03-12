6debfdc5a5c3b2c1dc1d04f8a17f87b7812dda7b
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:56:50 +0800

    Add files via upload

---
+++ c026.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring> 
+using namespace std;
+class CWin
+{
+public:
+    char id;
+    int width;
+    int height;
+    char title[20];
+
+    int area()
+    {
+        return width * height;
+    }
+    void set_title()
+    {
+        strcpy_s(title, "windows title");
+    }
+    void display()
+    {
+        cout << title;
+    }
+
+};
+int main()
+{
+    CWin win1;
+    win1.id = 'A';
+    win1.width = 50;
+    win1.height = 40;
+    win1.set_title();
+    cout << "Window " << win1.id << ":" << endl;
+    cout << "Area = " << win1.area() << endl;
+    win1.display();
+
+}
+
