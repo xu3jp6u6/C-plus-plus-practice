c75ed421187144810472ad367e6115f807487830
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:44:31 +0800

    Add files via upload

---
+++ c011.cpp
@@ -0,0 +1,25 @@
+﻿// c011.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num[5];
+    int i = 0;
+    double sum=0;
+    do
+    {
+        if (i == 5)
+        {
+            break;
+        }
+        cout << "input num :";
+        cin >> num[i];
+    } while (num[i++] > 0);
+    for (i = 0; i < 5; i++)
+        sum += num[i];
+    cout << "average =" << sum / 5;
+}
+
+
