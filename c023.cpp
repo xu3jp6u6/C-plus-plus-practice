d987bf32dd07cb35df67a0aac7147e27821e3c3c
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:55:22 +0800

    Add files via upload

---
+++ c023.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+using namespace std;
+class CBox
+{
+public:
+    int length;
+    int width;
+    int height;
+    int volume()
+    {
+        return length*width*height;
+    }
+    int surfaceArea()
+    {
+        return 2 * (length * width + length * height + width * height);
+    }
+};
+int main()
+{
+    CBox a;
+    a.length = 10;
+    a.width = 40;
+    a.height = 5;
+    cout << a.volume() << endl;
+    cout << a.surfaceArea() << endl;
+}
+
