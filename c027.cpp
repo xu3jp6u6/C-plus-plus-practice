c60e7af630eb495b0e103600243b4d0e2910b468
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:57:11 +0800

    Add files via upload

---
+++ c027.cpp
@@ -0,0 +1,34 @@
+﻿// c027.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+class CWin
+{   
+	char id;
+	int width;
+	int height;
+public:
+	void set_data(char i, int w, int h)
+	{
+		id = i;
+		width = w;
+		height = h;
+	}
+	friend int area(CWin);
+	friend void show_area(CWin);
+};
+int area(CWin w)
+{
+	return w.width * w.height;
+}
+void show_area(CWin win)
+{
+	cout << "window " << win.id << ",  area=" << area(win) << endl;
+}
+int main()
+{
+	CWin win1;
+	win1.set_data('B', 50,40);
+	show_area(win1);
+}
