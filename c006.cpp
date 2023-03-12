af756305cb86323eaa74eda24470964cf68507d0
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:30:18 +0800

    Add files via upload

---
+++ c006.cpp
@@ -0,0 +1,12 @@
+﻿// c006.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float f = 2.936f;
+	int i = 5;
+	cout << "f*i=" << f * i<<"\n"<< "f/i =" << f / i << endl;
+}
+
