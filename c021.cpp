f963a946c6d74cf8e7bd817dbb1078247839c3bf
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-12 16:54:32 +0800

    Add files via upload

---
+++ c021.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    char* ptr = str; // 宣告一個指向字元陣列的指標
+    cin.getline(str, 100);
+
+    // 將字符串轉換為大寫
+    while (*ptr != '\0')
+    {
+        if (*ptr==' ')
+        {
+            *ptr ='*';
+        }
+        ptr++;
+    }
+
+    cout << "替換后的字符串為：" << str << endl;
+
+}
