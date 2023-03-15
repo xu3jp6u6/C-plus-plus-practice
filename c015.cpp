a23077e3e2441732c745994f526e48ec876e6059
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-15 11:31:06 +0800

    Update c015.cpp

--- c015.cpp
+++ c015.cpp
@@ -6,16 +6,38 @@ using namespace std;
 void num(int*, int*, int*);
 int main()
 {
-	int a = 11, b = 9 ,c = 10;
+	int a , b ,c ;
+	cout << "a,b,c=" ;
+	cin >> a >>b >>c;
 	num(&a, &b, &c);
 	
 }
 void num(int *p1, int *p2, int *p3)
 {
+	cout << "由大到小排序 = ";
 	if (*p1 > *p2 && *p1 > *p3)
+	{
 		cout << *p1 << " ";
-	if (*p2 > *p3)
-		cout << *p2 <<" "<<*p3;
-	else
-		cout << *p3 << " " << *p2;
+		if (*p2 > *p3)
+			cout << *p2 << " " << *p3;
+		else
+			cout << *p3 << " " << *p2;
+	}
+	if (*p2 > *p1 && *p2 > *p3)
+	{
+		cout << *p2 << " ";
+		if (*p1 > *p3)
+			cout << *p1 << " " << *p3;
+		else
+			cout << *p3 << " " << *p1;
+	}
+	if (*p3 > *p1 && *p3 > *p2)
+	{
+		cout << *p3 << " ";
+		if (*p1 > *p2)
+			cout << *p1 << " " << *p2;
+		else
+			cout << *p2 << " " << *p1;
+	}
+
 }
